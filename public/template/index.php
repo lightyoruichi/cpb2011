2f9a1deec4d0e185b7b4d623713b23f255eca621
crynobone <crynobone@gmail.com>
2011-12-09 01:38:33 +0800

    Update asset loading
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ public/template/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Computing Professional Bill 2011</title>
+
+	<?php 
+
+	echo Asset::css(array('style.css', 'prettify.css', 'cpb2011.css'));
+	echo Asset::css(array('print.css'), array('media' => 'print', 'charset' => 'utf-8'));
+	echo Asset::js('prettify.js'); ?>
+</head>
+
+<body onload="prettyPrint()">
+	
+	<div id="wrapper"><!-- Begin wrapping -->
+		
+		<div id="header"><!-- Begin header -->
+			
+			<div id="masthead" class="left">
+				<h1>CPB2011</h1>
+			</div>
+			
+			<hr />
+			
+			<div id="count" class="right">
+			  <div>20 signatories</div>
+			</div>
+			
+			<div class="clear"></div>
+			
+			<hr />
+			
+		</div><!-- End header -->
+		
+		<div id="content"><!-- Begin content -->
+			
+			<div id="introduction" class="item">
+				
+				<h2><span class="item-title">Introduction</span> <strong><a href="#">Back to top</a></strong></h2>
+				<div class="item-content">
+				  <p>This petition site is merely a draft.</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis tristique magna quis lobortis. Aliquam imperdiet mi eget purus fringilla eu egestas metus cursus. Phasellus vulputate vehicula lorem, eget iaculis dui venenatis sit amet. Morbi imperdiet, nunc sit amet sollicitudin fringilla, elit sem auctor leo, eu feugiat nunc metus quis sem. Sed quam nulla, venenatis vitae varius at, viverra ut turpis. Sed condimentum nulla vel diam placerat sit amet venenatis lacus tincidunt. In sagittis dignissim laoreet. Vivamus pretium pulvinar elit, quis cursus erat tincidunt eu. Donec ultricies auctor nisl et placerat.</p>
+				</div>
+				
+			</div>
+			
+			<div id="features" class="item">
+				
+				<h2><span class="item-title">Issues</span> <strong><a href="#">Back to top</a></strong></h2>
+				<div class="item-content">
+				  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis tristique magna quis lobortis. Aliquam imperdiet mi eget purus fringilla eu egestas metus cursus. Phasellus vulputate vehicula lorem, eget iaculis dui venenatis sit amet. Morbi imperdiet, nunc sit amet sollicitudin fringilla, elit sem auctor leo, eu feugiat nunc metus quis sem. Sed quam nulla, venenatis vitae varius at, viverra ut turpis. Sed condimentum nulla vel diam placerat sit amet venenatis lacus tincidunt. In sagittis dignissim laoreet. Vivamus pretium pulvinar elit, quis cursus erat tincidunt eu. Donec ultricies auctor nisl et placerat.</p>
+				</div>
+				
+			</div>
+			
+			<div id="usage" class="item">
+				
+				<h2><span class="item-title">Petitions</span> <strong><a href="#">Back to top</a></strong></h2>
+				<div class="item-content">
+					<p>Opinions and commentaries can be forward to cpb2011 _at_ hq _dot_ my</p>
+				</div>
+				
+			</div>
+			
+			<div id="support" class="item">
+				
+				<h2><span class="item-title">Spread This</span> <strong><a href="#">Back to top</a></strong></h2>
+				<div class="item-content">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sagittis tristique magna quis lobortis. Aliquam imperdiet mi eget purus fringilla eu egestas metus cursus. Phasellus vulputate vehicula lorem, eget iaculis dui venenatis sit amet. Morbi imperdiet, nunc sit amet sollicitudin fringilla, elit sem auctor leo, eu feugiat nunc metus quis sem. Sed quam nulla, venenatis vitae varius at, viverra ut turpis. Sed condimentum nulla vel diam placerat sit amet venenatis lacus tincidunt. In sagittis dignissim laoreet. Vivamus pretium pulvinar elit, quis cursus erat tincidunt eu. Donec ultricies auctor nisl et placerat.</p>
+				</div>
+				
+			</div>
+						
+		</div><!-- End content -->
+		
+		<hr />
+		
+		<div id="footer"><!-- Begin footer -->
+			
+			<p class="left">&copy; 2011 CPB2011 Petition. This site is developed by uncertified web developer!</p>
+			<p class="right">Sponsored by <a href="http://mentariworks.com/">MentariWorks</a>.</p>
+			
+			<div class="clear"></div>
+			
+		</div><!-- End footer -->
+		
+	</div><!-- End wrapping -->
+	
+</body>
+</html>
